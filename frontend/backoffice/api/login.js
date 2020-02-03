a1c8ef78c88a548a85327e42c72a102c44e23b42
Lukasz Wojcik (PGS Software) <51819432+lwojcikpgs@users.noreply.github.com>
2020-02-03 09:37:11 +0100

    dsa dsa

--- frontend/backoffice/api/login.js
+++ frontend/backoffice/api/login.js
@@ -8,24 +8,29 @@ const router = Router();
 // var publicKEY  = fs.readFileSync('../cert/jwtRS256.key.pub', 'utf8');
 
 router.post('/', ({ body }, response) => {
-    // konopka.mateusz1989@gmail.com
-    const { login, password } = body;
-    if (ADMIN_LOGIN.split('|').find((a) => a === login) && password === ADMIN_PASSWORD) {
-
-        const token = jwt.sign({
-            login,
-            authorized: true,
-        }, JWT_SECRET, { algorithm: 'HS512', expiresIn: "7d"});
-        response.send({
-            login,
-            authorized: true,
-            token
-        })
-    } else {
-        response.status(400).send({
-            ADMIN_LOGIN, ADMIN_PASSWORD
-        });
-    }
+	// konopka.mateusz1989@gmail.com
+	const { login, password } = body;
+	if (ADMIN_LOGIN.split('|').find((a) => a === login) && password === ADMIN_PASSWORD) {
+		const token = jwt.sign(
+			{
+				login,
+				authorized: true
+			},
+			JWT_SECRET,
+			{ algorithm: 'HS512', expiresIn: '7d' }
+		);
+		response.send({
+			login,
+			authorized: true,
+			token
+		});
+	} else {
+		response.status(400).send({
+			ADMIN_LOGIN,
+			ADMIN_PASSWORD,
+			env: process.env
+		});
+	}
 });
 
 module.exports = router;

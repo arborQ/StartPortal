0807bde68a4302b3b454122f05d8e4bc61d29a5b
Lukasz Wojcik (PGS Software) <51819432+lwojcikpgs@users.noreply.github.com>
2020-02-03 18:42:15 +0100

    new version

--- RecognizePdf/PeselValidate/AccountCompare.xaml.cs
+++ RecognizePdf/PeselValidate/AccountCompare.xaml.cs
@@ -7,11 +7,15 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Windows;
+using System.Windows.Media;
 
 namespace PeselValidate
 {
     public class AccountListModel
     {
+        public static readonly string PADZOKON02 = "PADZOKON02";
+        public static readonly string PADKO00001 = "PADKO00001";
+
         public int Index { get; set; }
 
         public int StartPage { get; set; }
@@ -21,6 +25,75 @@ namespace PeselValidate
         public int ComparePage { get; set; }
 
         public string ClientName { get; set; } = "";
+
+        public bool HasClientName => !string.IsNullOrEmpty(ClientName);
+
+        public bool HasClientNameOnSecondPage { get; set; }
+
+        public bool HasAllPageIndex { get; set; }
+
+        public bool HasClientNameAfterPageIndex { get; set; }
+
+        public int ErrorCount => new[] {
+            !HasClientName ||
+            !HasClientNameOnSecondPage ||
+            !HasAllPageIndex ||
+            !HasClientNameAfterPageIndex }
+        .Where(a => a)
+            .Count();
+
+        public bool HasErrors => ErrorCount > 0;
+
+        public string PagesRange => $"Strony {StartPage}/{EndPage}";
+
+        public SolidColorBrush Color
+        {
+            get
+            {
+                if (HasErrors)
+                {
+                    SolidColorBrush brush = new SolidColorBrush();
+                    brush.Color = Colors.Red;
+                    return brush;
+                }
+                else
+                {
+                    SolidColorBrush brush = new SolidColorBrush();
+                    brush.Color = Colors.Black;
+                    return brush;
+                }
+            }
+        }
+
+        public string Summary
+        {
+            get
+            {
+                var sb = new StringBuilder();
+
+                if (!HasClientName)
+                {
+                    return "Nie znalazłem nazwy klienta!";
+                }
+
+                if (!HasClientNameOnSecondPage)
+                {
+                    sb.AppendLine("Nie ma nazwy klienta na drugiej stronie!");
+                }
+
+                if (!HasAllPageIndex)
+                {
+                    sb.AppendLine($"Nie wszystkie strony zestawienia zawierają {PADZOKON02} lub {PADKO00001}");
+                }
+
+                if (!HasClientNameAfterPageIndex)
+                {
+                    sb.AppendLine("Strona po zestawieniu nie zawiera imienia klienta");
+                }
+
+                return sb.ToString();
+            }
+        }
     }
 
     /// <summary>
@@ -36,6 +109,23 @@ namespace PeselValidate
             PdfDocumentText = new string[0];
             LoadedRecords = new AccountListModel[0];
             InitializeComponent();
+
+            //ResultList.Items.Clear();
+            //ResultList.Items.Add(new AccountListModel
+            //{
+            //    ClientName = "Łukasz Wójcik",
+            //    StartPage = 10,
+            //    EndPage = 20
+            //});
+            //ResultList.Items.Add(new AccountListModel
+            //{
+            //    ClientName = "Ola Wójcik",
+            //    StartPage = 10,
+            //    EndPage = 20,
+            //    HasAllPageIndex = true,
+            //    HasClientNameAfterPageIndex = true,
+            //    HasClientNameOnSecondPage = true,
+            //});
         }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e)
@@ -114,6 +204,7 @@ namespace PeselValidate
         private void RecalculateData()
         {
             ResultList.Items.Clear();
+            var resultList = new List<AccountListModel>();
             foreach (var item in LoadedRecords)
             {
                 try
@@ -123,111 +214,96 @@ namespace PeselValidate
                         .Take(item.EndPage - item.StartPage)
                         .ToArray();
 
-                    if (clientPages.Any())
+                    var clientName = clientPages[0]
+                        .ReadLineByLine()
+                        .Skip(4)
+                        .FirstOrDefault();
+
+                    if (!item.HasClientName)
                     {
-                        // czy page 1 jest pusty
-                        // czy page 2, linia 5 ma imie
-                        // PADZOKONO2 PADKO00001
-                        // czy page  6, linia 4 ma imie
-                        // czy page 6, Prowadzono dla: imie
-                        var clientName = clientPages[0]
-                            .ReadLineByLine()
-                            .Skip(4)
-                            .FirstOrDefault();
-
-                        if (string.IsNullOrEmpty(clientName))
-                        {
-                            item.ClientName = "Nie znalazłem danych klienta!";
-                            continue;
-                        }
-
-                        var sb = new StringBuilder();
-                        sb.Append($"Nazwa: {clientName} ");
-
-                        var linesWithNames = clientPages
-                            .SelectMany((cp, i) => LinesWithNames(cp.ReadLineByLine().ToArray(), clientName)
-                            .Select(c => $"S:{i},L:{c}"))
-                            .ToArray();
-
-                        var pagesWithNameIndex = clientPages
-                           .Select((cp, i) => LinesWithNames(cp.ReadLineByLine().ToArray(), clientName).Any() ? i : -1)
-                           .Where(p => p > 0)
-                           .ToArray();
-
-                        foreach(var indexName in pagesWithNameIndex)
-                        {
-                            var path = Path.Combine(DocumentPath, $"{clientName}_{indexName}.txt");
-                            File.WriteAllText(path, clientPages[indexName]);
-                        }
-
-                       sb.Append(string.Join(", ", linesWithNames) + " ");
-
-                        if (ContainsName(clientPages[2].ReadLineByLine().ToArray(), 5, clientName))
-                        {
-                            sb.Append($"Druga strona zawiera imię ");
-                        }
-                        else
-                        {
-                            sb.Append($"Druga NIE strona zawiera imienia ");
-                        }
-                        var findTokenExpression = new Func<string, bool>((pageText) => pageText.Contains("PADZOKON02") || pageText.Contains("PADKO00001"));
-                        var findTokenPredicate = new Predicate<string>(pageText => findTokenExpression(pageText));
-
-                        var orderPageFirstIndex = Array
-                            .FindIndex(
-                            clientPages,
-                            findTokenPredicate);
-
-                        var orderPageLastIndex = Array
-                            .FindLastIndex(
-                            clientPages,
-                            findTokenPredicate);
-
-                        var allTokens = clientPages
-                            .Skip(orderPageFirstIndex)
-                            .Take(orderPageLastIndex - orderPageFirstIndex)
-                            .All(findTokenExpression);
-
-                        if (allTokens)
-                        {
-                            sb.Append($"Wszystkie strony od {item.StartPage + orderPageFirstIndex} do {item.StartPage + orderPageLastIndex} mają index zestawienia ");
-                        }
-
-
-                        if (clientPages.Length > orderPageLastIndex + 3)
-                        {
-                            if (ContainsName(clientPages[orderPageLastIndex + 2].ReadLineByLine().ToArray(), 10, clientName))
-                            {
-                                sb.Append($"{item.StartPage + orderPageLastIndex + 2} strona zawiera imię ");
-                            }
-                            else
-                            {
-                                sb.Append($"{item.StartPage + orderPageLastIndex + 2} NIE strona zawiera imienia ");
-                            }
-                        }
-                        else
-                        {
-                            var path = Path.Combine(DocumentPath, $"{clientName}.txt");
-                            File.WriteAllText(path, clientPages[orderPageLastIndex + 2]);
-                        }
-
-                        item.ClientName = sb.ToString();
-                    }
-                    else
-                    {
-                        item.ClientName = "Nie ma odpowiedniej strony";
+                        continue;
                     }
 
-                    ResultList.Items.Add(item);
+                    var sb = new StringBuilder();
+
+                    var linesWithNames = clientPages
+                        .SelectMany((cp, i) => LinesWithNames(cp.ReadLineByLine().ToArray(), clientName)
+                        .Select(c => $"S:{i},L:{c}"))
+                        .ToArray();
+
+                    var pagesWithNameIndex = clientPages
+                       .Select((cp, i) => LinesWithNames(cp.ReadLineByLine().ToArray(), clientName).Any() ? i : -1)
+                       .Where(p => p > 0)
+                       .ToArray();
+
+                    foreach (var indexName in pagesWithNameIndex)
+                    {
+                        var path = Path.Combine(DocumentPath, $"{clientName}_{indexName}.txt");
+                        File.WriteAllText(path, clientPages[indexName]);
+                    }
+
+                    sb.Append(string.Join(", ", linesWithNames) + " ");
+                    item.HasClientNameOnSecondPage = ContainsName(clientPages[2].ReadLineByLine().ToArray(), 5, clientName);
+
+                    var findTokenExpression = new Func<string, bool>((pageText) => pageText.Contains(AccountListModel.PADZOKON02) || pageText.Contains(AccountListModel.PADKO00001));
+                    var findTokenPredicate = new Predicate<string>(pageText => findTokenExpression(pageText));
+
+                    var orderPageFirstIndex = Array
+                        .FindIndex(
+                        clientPages,
+                        findTokenPredicate);
+
+                    var orderPageLastIndex = Array
+                        .FindLastIndex(
+                        clientPages,
+                        findTokenPredicate);
+
+                    var allTokens = clientPages
+                        .Skip(orderPageFirstIndex)
+                        .Take(orderPageLastIndex - orderPageFirstIndex)
+                        .All(findTokenExpression);
+
+
+                    item.HasAllPageIndex = allTokens;
+                    //if (allTokens)
+                    //{
+                    //    sb.Append($"Wszystkie strony od {item.StartPage + orderPageFirstIndex} do {item.StartPage + orderPageLastIndex} mają index zestawienia ");
+                    //}
+
+                    item.HasClientNameAfterPageIndex = ContainsName(clientPages[orderPageLastIndex + 2].ReadLineByLine().ToArray(), 10, clientName);
+                    //if (clientPages.Length > orderPageLastIndex + 3)
+                    //{
+                    //    if (ContainsName(clientPages[orderPageLastIndex + 2].ReadLineByLine().ToArray(), 10, clientName))
+                    //    {
+                    //        sb.Append($"{item.StartPage + orderPageLastIndex + 2} strona zawiera imię ");
+                    //    }
+                    //    else
+                    //    {
+                    //        sb.Append($"{item.StartPage + orderPageLastIndex + 2} NIE strona zawiera imienia ");
+                    //    }
+                    //}
+                    //else
+                    //{
+                    //    var path = Path.Combine(DocumentPath, $"{clientName}.txt");
+                    //    File.WriteAllText(path, clientPages[orderPageLastIndex + 2]);
+                    //}
+
+                    //item.ClientName = sb.ToString();
+
+                    resultList.Add(item);
                 }
                 catch (Exception e)
                 {
-                    item.ClientName = e.Message;
-                    ResultList.Items.Add(item);
-                    Title = e.Message;
+                    item.ClientName = e.Message + e.StackTrace;
+                    resultList.Add(item);
                 }
             }
 
+            ResultList.Items.Clear();
+            foreach (var item in resultList.OrderByDescending(r => r.ErrorCount))
+            {
+                ResultList.Items.Add(item);
+            }
         }
     }
 }

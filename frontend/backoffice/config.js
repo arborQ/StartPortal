aa839222ae9839f144ed104bd1b2e07acf8cd4c2
Lukasz Wojcik (PGS Software) <lwojcik@pgs-soft.com>
2020-02-04 21:38:22 +0100

    add proxy

--- frontend/backoffice/config.js
+++ frontend/backoffice/config.js
@@ -1,8 +1,9 @@
 const envalid = require('envalid');
-const { port } = envalid;
+const { port, url } = envalid;
 
 const params = {
-	PORT: port({ devDefault: 8080 })
+	PORT: port({ devDefault: 8080 }),
+	PROXY_ACCOUNT: url({ devDefault: 'http://localhost:8080/api/**' })
 };
 
 const env = envalid.cleanEnv(process.env, params);
--- frontend/backoffice/server.js
+++ frontend/backoffice/server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const path = require('path');
+var proxyMiddleware = require('http-proxy-middleware');
 const app = express();
-const { PORT: port } = require('./config');
+const { PORT: port, PROXY_ACCOUNT } = require('./config');
 
+app.use(proxyMiddleware(PROXY_ACCOUNT));
 
 app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', function (_, res) {

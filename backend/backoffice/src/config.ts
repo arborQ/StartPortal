810829e6000f41e19fba0fa2c01807cf5cc7ec28
Lukasz Wojcik (PGS Software) <lwojcik@pgs-soft.com>
2020-02-18 22:00:09 +0100

    765756

--- backend/backoffice/src/config.ts
+++ backend/backoffice/src/config.ts
@@ -2,12 +2,12 @@ import envalid, { url } from 'envalid';
 const { str, port } = envalid;
 
 const params = {
-	PORT: port({ default: 8080 }),
-	ADMIN_LOGIN: str({ default: 'admin' }),
-	ADMIN_PASSWORD: str({ default: 'admin' }),
-	JWT_SECRET: str({ default: 'secret' }),
+	PORT: port({ devDefault: 4000 }),
+	ADMIN_LOGIN: str({ devDefault: 'admin' }),
+	ADMIN_PASSWORD: str({ devDefault: 'admin' }),
+	JWT_SECRET: str({ devDefault: 'secret' }),
 	MONGO_CONNECTION_STRING: url(),
-	LOGIN_EXPIRE: str({ default: '7d' })
+	LOGIN_EXPIRE: str({ devDefault: '7d' })
 };
 
 export default envalid.cleanEnv(process.env, params);
--- frontend/backoffice/config.js
+++ frontend/backoffice/config.js
@@ -2,8 +2,8 @@ const envalid = require('envalid');
 const { port, url } = envalid;
 
 const params = {
-	PORT: port({ devDefault: 8080 }),
-	PROXY_ACCOUNT: url({ devDefault: 'http://localhost:8080/api/**' })
+	PORT: port({ devDefault: 3000 }),
+	PROXY_ACCOUNT: url({ devDefault: 'http://backoffice_api:4000' })
 };
 
 const env = envalid.cleanEnv(process.env, params);
